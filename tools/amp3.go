c68263d6df10fefbcb464c18f19c5e185a237171
Yuichi KAWAMATA <kawamata@Sirius.local>
2019-07-03 21:09:32 +0900

    update amp3 over

--- tools/amp3.go
+++ tools/amp3.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"strconv"
 )
 
@@ -11,15 +12,18 @@ import (
 // ./amp3 * 
 
 func main() {
-	fmt.Println("AUTO FFMPEG TO MP3 v1.0")
+	fmt.Println("AUTO FFMPEG TO MP3 v1.1")
 	fmt.Println("Arg count : ", len(os.Args) - 1)
 
 	for i := 1; i < len(os.Args); i++ {
 		fmt.Println(os.Args[i])
 		fmt.Println(strconv.Itoa(i))
-		err := exec.Command("ffmpeg", "-i", os.Args[i], strconv.Itoa(i) + ".mp3").Run()
-			if err != nil {
-				fmt.Println("ERR")
-			}
+
+		err := exec.Command("ffmpeg", "-i", os.Args[i],
+			strings.Split(os.Args[i], ".")[0] + ".mp3").Run()
+
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
