1ed1f447a7791c4c0bb8e630e43d41cdb926938b
Yuichi KAWAMATA <kawamata@Sirius.local>
2019-07-03 20:33:21 +0900

    aydtool launch

---
+++ tools/aydtool.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"io/ioutil"
+	"strings"
+)
+
+/*format*/
+// ./aydtool list 
+
+func main() {
+	fmt.Println("RE NAME TOOL v1.0")
+	fmt.Println("List :", os.Args[1])
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("ERROR")
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	urls := strings.Split(string(b), "\n")
+
+	for i := 0; i < len(urls); i++ {
+		if len(urls[i]) < 3 {
+			continue
+		}
+
+		fmt.Println(i + 1, "/", len(urls), urls[i])
+		err = exec.Command("youtube-dl", urls[i]).Run()
+
+		if err != nil {
+			fmt.Println("ERR youtube-dl")
+		}
+	}
+}	
